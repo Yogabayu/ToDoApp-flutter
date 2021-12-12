9981f3e389edca5a0a0a822c2514802242ee380a
Yoga Bayu AP <yogabayusbi@gmail.com>
2021-12-12 13:02:16 +0700

    master

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -2,13 +2,10 @@ import 'package:flutter/material.dart';
 // import 'package:get/get.dart';
 import 'package:todoapp_1/constant.dart';
 import 'package:fab_circular_menu/fab_circular_menu.dart';
+import 'package:todoapp_1/helper/sql_helper.dart';
 
 String nama = "Yoga Bayu";
 final GlobalKey<FabCircularMenuState> fabKey = GlobalKey();
-final List<String> _todoList = <String>[];
-// text field
-final TextEditingController _textFieldController = TextEditingController();
-final TextEditingController _namaFieldController = TextEditingController();
 
 class Dashboard extends StatefulWidget {
   const Dashboard({Key? key}) : super(key: key);
@@ -18,9 +15,120 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+// All journals
+  List<Map<String, dynamic>> _journals = [];
+
+  bool _isLoading = true;
+  // This function is used to fetch all data from the database
+  void _refreshJournals() async {
+    final data = await SQLHelper.getItems();
+    setState(() {
+      _journals = data;
+      _isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _refreshJournals(); // Loading the diary when the app starts
+  }
+
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+
+  // This function will be triggered when the floating button is pressed
+  // It will also be triggered when you want to update an item
+  void _showForm(int? id) async {
+    if (id != null) {
+      // id == null -> create new item
+      // id != null -> update an existing item
+      final existingJournal =
+          _journals.firstWhere((element) => element['id'] == id);
+      _titleController.text = existingJournal['title'];
+      _descriptionController.text = existingJournal['description'];
+    }
+
+    showModalBottomSheet(
+        context: context,
+        elevation: 5,
+        builder: (_) => Container(
+              padding: const EdgeInsets.all(15),
+              width: double.infinity,
+              height: 300,
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    TextField(
+                      controller: _titleController,
+                      decoration: const InputDecoration(hintText: 'Title'),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextField(
+                      controller: _descriptionController,
+                      decoration:
+                          const InputDecoration(hintText: 'Description'),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                      onPressed: () async {
+                        // Save new journal
+                        if (id == null) {
+                          await _addItem();
+                        }
+
+                        if (id != null) {
+                          await _updateItem(id);
+                        }
+
+                        // Clear the text fields
+                        _titleController.text = '';
+                        _descriptionController.text = '';
+
+                        // Close the bottom sheet
+                        Navigator.of(context).pop();
+                      },
+                      child: Text(id == null ? 'Create New' : 'Update'),
+                    )
+                  ],
+                ),
+              ),
+            ));
+  }
+
+// Insert a new journal to the database
+  Future<void> _addItem() async {
+    await SQLHelper.createItem(
+        _titleController.text, _descriptionController.text);
+    _refreshJournals();
+  }
+
+  // Update an existing journal
+  Future<void> _updateItem(int id) async {
+    await SQLHelper.updateItem(
+        id, _titleController.text, _descriptionController.text);
+    _refreshJournals();
+  }
+
+  // Delete an item
+  void _deleteItem(int id) async {
+    await SQLHelper.deleteItem(id);
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+      content: Text('Successfully deleted a journal!'),
+    ));
+    _refreshJournals();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: SingleChildScrollView(
         physics: ClampingScrollPhysics(),
         child: Column(
@@ -38,13 +146,11 @@ class _DashboardState extends State<Dashboard> {
                   child: Column(
                     mainAxisSize: MainAxisSize.max,
                     mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Row(),
                       Column(
-                        mainAxisSize: MainAxisSize.min,
-                        mainAxisAlignment: MainAxisAlignment.center,
                         crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
+                        children: <Widget>[
                           SizedBox(
                             height: MediaQuery.of(context).size.height * 0.1,
                           ),
@@ -175,31 +281,64 @@ class _DashboardState extends State<Dashboard> {
                           Padding(
                             padding: paddingCol,
                             child: Text(
-                              "ToDo : ",
+                              "Tes : ",
                               textAlign: TextAlign.left,
                               style: TextStyle(
                                   fontSize: 20, fontWeight: FontWeight.bold),
                             ),
                           ),
-                          SizedBox(
-                            height: 30,
-                          ),
-                          // Padding(
-                          //   padding: const EdgeInsets.all(8.0),
-                          //   child: Expanded(
-                          //       child: SizedBox(
-                          //           height: 50,
-                          //           child: ListView(children: _getItems()))),
-                          // )
                           Padding(
-                              padding: paddingCol,
-                              child: Column(
-                                  mainAxisSize: MainAxisSize.max,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: <Widget>[
-                                    Expanded(
-                                        child: ListView(children: _getItems()))
-                                  ]))
+                            padding: paddingCol,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.blueAccent)),
+                              height: 400,
+                              child: _isLoading
+                                  ? const Center(
+                                      child: CircularProgressIndicator(),
+                                    )
+                                  : SingleChildScrollView(
+                                      child: ListView.builder(
+                                        scrollDirection: Axis.vertical,
+                                        shrinkWrap: true,
+                                        physics: NeverScrollableScrollPhysics(),
+                                        itemCount: _journals.length,
+                                        itemBuilder: (context, index) => Card(
+                                          color: Colors.orange[200],
+                                          margin: const EdgeInsets.all(15),
+                                          child: ListTile(
+                                              title: Text(
+                                                  _journals[index]['title']),
+                                              subtitle: Text(_journals[index]
+                                                  ['description']),
+                                              trailing: SizedBox(
+                                                width: 100,
+                                                child: Row(
+                                                  children: [
+                                                    IconButton(
+                                                      icon: const Icon(
+                                                          Icons.edit),
+                                                      onPressed: () =>
+                                                          _showForm(
+                                                              _journals[index]
+                                                                  ['id']),
+                                                    ),
+                                                    IconButton(
+                                                      icon: const Icon(
+                                                          Icons.delete),
+                                                      onPressed: () =>
+                                                          _deleteItem(
+                                                              _journals[index]
+                                                                  ['id']),
+                                                    ),
+                                                  ],
+                                                ),
+                                              )),
+                                        ),
+                                      ),
+                                    ),
+                            ),
+                          ),
                         ],
                       ),
                     ],
@@ -234,7 +373,8 @@ class _DashboardState extends State<Dashboard> {
           children: <Widget>[
             RawMaterialButton(
               onPressed: () {
-                _displayDialog(context);
+                _showForm(null);
+                fabKey.currentState!.close();
               },
               shape: CircleBorder(),
               padding: const EdgeInsets.all(24.0),
@@ -269,69 +409,5 @@ class _DashboardState extends State<Dashboard> {
         ),
       ),
     );
-  }
-
-  // void _showSnackBar(BuildContext context, String message) {
-  //   // ignore: deprecated_member_use
-  //   Scaffold.of(context).showSnackBar(SnackBar(
-  //     content: Text(message),
-  //     duration: const Duration(milliseconds: 1000),
-  //   ));
-  // }
-
-  void _addTodoItem(String title) async {
-    // Wrapping it inside a set state will notify
-    // the app that the state has changed
-    setState(() {
-      _todoList.add(title);
-    });
-    _textFieldController.clear();
-  }
-
-  // this Generate list of item widgets
-  Widget _buildTodoItem(String title) {
-    return ListTile(title: Text(title));
-  }
-
-  // display a dialog for the user to enter items
-  Future<void> _displayDialog(BuildContext context) async {
-    // alter the app state to show a dialog
-    return showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: const Text('Add a task to your list'),
-            content: TextField(
-              controller: _textFieldController,
-              decoration: const InputDecoration(hintText: 'Enter task here'),
-            ),
-            actions: <Widget>[
-              // add button
-              TextButton(
-                child: const Text('ADD'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                  _addTodoItem(_textFieldController.text);
-                },
-              ),
-              // Cancel button
-              TextButton(
-                child: const Text('CANCEL'),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-              )
-            ],
-          );
-        });
-  }
-
-  // iterates through our todo list title
-  List<Widget> _getItems() {
-    final List<Widget> _todoWidgets = <Widget>[];
-    for (String title in _todoList) {
-      _todoWidgets.add(_buildTodoItem(title));
-    }
-    return _todoWidgets;
   }
 }
